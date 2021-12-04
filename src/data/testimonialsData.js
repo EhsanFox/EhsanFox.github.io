303192f71e067d0d305e5a26934fdd1ec7844927
EhsanFox <ehsan8184@gmail.com>
2021-12-04 04:06:39 +0330

    🎉 | might use them

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -1,10 +1,10 @@
 /* eslint-disable */
-//import girl1 from '../assets/svg/testimonials/girl1.svg'
-//import girl2 from '../assets/svg/testimonials/girl2.svg'
-//import girl3 from '../assets/svg/testimonials/girl3.svg'
-//import boy1 from '../assets/svg/testimonials/boy1.svg'
-//import boy2 from '../assets/svg/testimonials/boy2.svg'
-//import boy3 from '../assets/svg/testimonials/boy3.svg'
+import girl1 from '../assets/svg/testimonials/girl1.svg'
+import girl2 from '../assets/svg/testimonials/girl2.svg'
+import girl3 from '../assets/svg/testimonials/girl3.svg'
+import boy1 from '../assets/svg/testimonials/boy1.svg'
+import boy2 from '../assets/svg/testimonials/boy2.svg'
+import boy3 from '../assets/svg/testimonials/boy3.svg'
 import hadi from '../assets/svg/testimonials/hadi.svg'
 
 
