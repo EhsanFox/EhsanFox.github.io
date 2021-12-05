897e524ad40f2be7976f18e72739bb51a71bc9f4
EhsanFox <ehsan8184@gmail.com>
2021-12-05 06:57:15 +0330

    ✨| New testimonial

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -6,7 +6,7 @@ import boy1 from '../assets/svg/testimonials/boy1.svg'
 import boy2 from '../assets/svg/testimonials/boy2.svg'
 import boy3 from '../assets/svg/testimonials/boy3.svg'
 import hadi from '../assets/svg/testimonials/hadi.svg'
-
+import parsa from '../assets/svg/testimonials/parsa.svg'
 
 
 export const testimonialsData = [
@@ -17,5 +17,12 @@ export const testimonialsData = [
         text: "The best person that you can trust with projects, i know he would do the best he can.",
         image: hadi
     },
+    {
+        id: 2,
+        name: 'Parsa Firoozi',
+        title: 'Co-Worker',
+        text: "One of the best back-and developers I've ever seen. We did not do many projects together, but in the same few I got a very good feeling of his behavior and code-style.",
+        image: parsa
+    }
 ]
 
