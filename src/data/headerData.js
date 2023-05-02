8b422957305377ddcf3bfee88d6ab59c6b3c20ea
EhsanFox <EhsanFox@users.noreply.github.com>
2023-05-02 10:55:07 +0330

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,4 +1,4 @@
-import resume from '../assets/pdf/resume.pdf'
+
 import profile from '../assets/jpg/profile.jpg'
 
 export const headerData = {
@@ -6,5 +6,5 @@ export const headerData = {
     title: "Back-End Developer",
     desciption:"a Boy with Big Dreams and UnBelievable ideas in his head. Ready to make anything possible, with lots of love in IoT and Automation Algorithms. Can't stop thinking about making Practical Applications.",
     image: profile,
-    resumePdf: resume
+    resumePdf: ""
 }
