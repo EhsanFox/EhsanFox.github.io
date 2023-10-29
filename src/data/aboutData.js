139e73c44fb203e6b2b41c6e2d0db0ba8e0ed24c
Ehsan Golmakani <EhsanFox@users.noreply.github.com>
2023-10-29 09:02:47 +0330

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who am i ?",
-    description1: "My name is Ehsan. I'm a Back-End Developer based in Mashhad, Iran.",
+    description1: "My name is Ehsan. I'm a Back-End Developer, from Stockholm, Sweden.",
     description2: "During the day, I'm working on my Freelance Jobs and Projects, and during the evening, I'm working on my own branding and a *secret* application.",
     image: 1
 }
